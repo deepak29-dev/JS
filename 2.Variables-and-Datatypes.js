1475f53c9bced82cde013e7fd57ceadfaf625dfe
Deepak Singh <deepakxsingh91@gmail.com>
2025-12-27 10:22:25 +0530

    Complete of  lecture2

---
+++ 2.Variables-and-Datatypes.js
@@ -0,0 +1,85 @@
+// Declaring Variables
+let name = "Deepak ";
+let age = 20;
+age =21;
+console.log("Name :" ,name,age);
+//const means it will not change
+ const Number=32;
+//   a=21;
+ console.log(Number);
+
+
+
+
+//  *************DataTypes*****************
+
+
+// Primitvive DT: Number,string,boolean,undefined,bigint,null,Symbol
+// Non Primitvit DT:Array,function etc
+
+//Number
+
+let a = 10;
+let b = 2.33;
+console.log(a,b);
+
+//String
+
+let c = 'My name is : ';
+let d = 'Deepak ';
+console.log(c,d);
+
+//Boolen
+
+let login = true;
+let f =false ;
+console.log(login,f);
+
+//Undefined
+
+let user ;
+console.log(user);
+
+//Bigint
+
+let num=23456789182928n;
+console.log(num);
+
+// Null
+
+let weather = null;
+console.log(weather);
+
+//Symbol
+
+const id1 = Symbol("id");
+console.log(id1);
+
+
+// Non Primitvie Datatype
+
+//array
+let arr = [10,20,11,"Deepak",true];
+console.log(arr);
+// Object 
+let users={
+    name:"Deepak",
+    account : 12113,
+    age:20,
+    category:"GEN"
+}
+//function
+function add(){
+    console.log("HEllo");
+
+}
+add();
+
+// we can store the function in variable
+
+
+//Primitvie datatype is immutable means it cannot be manupulated
+// NOn primitive datatype is mutable means it can be manupulated
+
+
+
